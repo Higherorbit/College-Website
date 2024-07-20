279016fb9dee77109adb98dda8201f99390d5bdd
Samandeep Singh <f20200065@hyderabad.bits-pilani.ac.in>
2024-07-20 19:47:55 +0530

    First commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
 export default function App() {
-  return <h1 className='text-red-1000'> sup  </h1>
+  
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', height: '100vh' }}>
+      <h1 className='text-red-1000' style={{ fontSize: '2rem' }}>College Website </h1>
+    </div>
+  );
 }
