9cf41c82e849512aec4c12afa8dce54a40609c0d
Samandeep Singh <f20200065@hyderabad.bits-pilani.ac.in>
2024-07-23 00:28:35 +0530

    Created and ran server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Header() {
 return (
     <div className='bg-slate-200'>
-        <div className='container mx-auto flex justify-between items-center max-w-6xl mx-auto p-4'>
+        <div className='container flex justify-between items-center mx-auto max-w-6xl mx-auto p-4'>
             <Link to='/' className='text-2xl font-bold'>College Website</Link>
             <div className='flex items-center font-bold gap-4'>
                 <Link to='/about'>About</Link>
