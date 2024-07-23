bf6c9b84eb1a8a76143b642e66c2f328bfa3e2fc
Samandeep Singh <f20200065@hyderabad.bits-pilani.ac.in>
2024-07-23 16:06:19 +0530

    Created test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 dotenv.config();
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO).then(() => {
@@ -17,3 +18,5 @@ app.listen(3002, () => {
     console.log('Server is running on port 3002!');
     }
 );
+
+app.use("/api/user",userRoutes);
